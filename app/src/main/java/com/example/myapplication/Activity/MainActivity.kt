457a2ff5f4df367a94aec970e303617f2cb34288
somanita019 <somanita019@gmail.com>
2025-01-06 09:19:29 +0700

    impliment on history and history order detail

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -23,15 +24,20 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.logout -> {
-                // Log out from Firebase
                 FirebaseAuth.getInstance().signOut()
-                // Intent to navigate to Sign-in activity
                 val intent = Intent(this, SignInActivity::class.java)
                 startActivity(intent)
                 finish()
                 true
             }
+            R.id.history -> {
+                Log.d("MainActivity", "History menu item selected")
+                val intent = Intent(this, HistoryActivity::class.java)
+                startActivity(intent)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
